63d13ae515f8eea69d16a1efacd1c4070ef6503a
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-08-22 04:37:03 +0300

    add-hover-for-button-and-change-move-witch

--- src/game-page.js
+++ src/game-page.js
@@ -1,31 +1,33 @@
 const witch = document.getElementById('witch');
-const fireball = document.getElementById('fire');
+const fire = document.getElementById('fire');
 const arrow = document.getElementById('arrow');
 
-console.log(fireball);
-console.log(witch);
+console.log(fire);
+// console.log(witch.style.animation = "wMove 500ms linear 1s infinite");
 
 document.addEventListener('keydown', function keydown(event) {
   jump();
 });
+witch.classList.add('move');
 
 function jump() {
-  if (witch.classList != 'jump') { 
-    witch.classList.add('jump');  
+  if (witch.classList != 'jump') {
+    witch.classList.add('jump');
+    witch.classList.remove('move');
   }
   setTimeout(function () {
-      witch.classList.remove('jump');
-    }, 600);
-    
+    witch.classList.add('move');
+    witch.classList.remove('jump');
+  }, 600);
 }
 
 // let isAlive = setInterval(
 //     function life(){
-//         let fireballRight = parseInt(window.getComputedStyle(fireball).getPropertyValue('right'))
+//         let fireRight = parseInt(window.getComputedStyle(fire).getPropertyValue('right'))
 //         let arrowRight = parseInt(window.getComputedStyle(arrow).getPropertyValue('right'))
 //         let witchBottom = parseInt(window.getComputedStyle(witch).getPropertyValue('bottom'))
 
-//         if(fireballRight < 750 && fireballRight > 700 && witchBottom < 20 || arrowRight < 750 && arrowRight > 730 && witchBottom < 20){
+//         if(fireRight < 825 && fireRight > 780 && witchBottom < 20 || arrowRight < 812 && arrowRight > 767 && witchBottom < 20){
 //             window.alert("GAME OVER !!!")
 //         }
 //     }
